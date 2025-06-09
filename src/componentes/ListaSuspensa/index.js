a63fb86b291d49ae4465449d8259177872b096f9
Cesar Augusto Tomm <czar.df.69@gmail.com>
2025-06-08 23:59:35 -0300

    refatorar campo-texto > campo, ajustes no css

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -1,6 +1,6 @@
 import './lista-suspensa.css'
 
-const ListaSuspensa = ({label, items,  valor, aoAlterado, obrigatorio = false}) => {
+const ListaSuspensa = ({label, items,  valor, aoAlterado, obrigatorio=false}) => {
     return (<div className="lista-suspensa">
         <label>{label}</label>
         <select required={obrigatorio} value={valor} onChange={evento => aoAlterado(evento.target.value)}>
